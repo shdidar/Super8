d1be0ee4fb06b8444f0119826c1a00d1ac5efb13
shdid <shdid@10.67.105.192>
2021-05-15 16:39:01 -0400

    first commit

---
+++ src/assignment4/Multiply.java
@@ -0,0 +1,21 @@
+package assignment4;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		
+		int a =35, b = 75, c;
+		
+		c= a*b;
+		
+		System.out.println("Product of Two Numbers In Java Is -->>");
+		
+		System.out.println(c);
+		 
+		
+		
+		
+
+	}// end main
+
+}
---
+++ src/interm/Addition.java
@@ -0,0 +1,23 @@
+package interm;
+
+public class Addition {
+
+	public static void main(String[] args) {
+
+		//instance variable
+		int x;
+		int y;
+		void add() {
+			
+			int sum; // local variable
+			
+			sum = x + y;
+			
+					System.out.println();
+			
+		} // end of a add
+		
+
+	}
+
+}
